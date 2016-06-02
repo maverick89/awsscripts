3a2e6840bfb2e121d7473da1a29b5941a21ed4db
truptesh <truptesh@amazon.com>
2016-06-02 14:49:47 -0400

    Fixed the issue where Instance Name was not fetched when there were multiple Tags. Added region name validation

--- get-instance-list.py
+++ get-instance-list.py
@@ -6,15 +6,18 @@ region = (sys.argv)
 
 defaultRegion = "us-east-1"
 
+listOfRegions = ["us-east-1","us-west-1","us-west-2","eu-west-1","eu-central-1","sa-east-1","ap-southeast-1","ap-southeast-2","ap-northeast-1","ap=northeasr-2"]
+
 if len(region)==1:
+	print "No region specified, using default region us-east-1"
 	region.append(defaultRegion)
 
-try:
-	ec2 = boto3.resource("ec2",region_name=region[1])
+if len(region)>1:
+	if region[1] not in listOfRegions:
+		print "Unrecognised region name"
+		sys.exit(0)
 
-except ValueError:
-	print "Invalid region name"
-	sys.exit(0)
+ec2 = boto3.resource("ec2",region_name=region[1])
 
 
 try:
@@ -25,15 +28,12 @@ except botocore.exceptions.EndpointConnectionError:
 	sys.exit(0)	
 
 instanceList = []
-i =0
 for instance in instances:
 	templist = [instance.id, instance.instance_type, instance.tags, instance.private_ip_address, instance.public_ip_address, instance.key_name]
-
 	instanceList.append(templist)
-	i = i+1
-#print instanceList[0][0]
 
 lengthOfList = len(instanceList)
+
 print "Total Number of Instances: " + str(lengthOfList);
 
 print '\033[1m' + " ======================================================================================================================================"
@@ -41,8 +41,8 @@ print "| " + '{:22}'.format("Instance Id")+"| "+'{:30}'.format("Name")+"| "+'{:1
 print " ======================================================================================================================================" + '\033[0m'
 
 for x in range(0,len(instanceList)):
-
-	print "| " + '{:22}'.format(instanceList[x][0]) + "| "+'{:30}'.format(instanceList[x][2][0]["Value"])+"| " +'{:18}'.format(instanceList[x][4])+"| "+'{:18}'.format(instanceList[x][3])+"| "+'{:15}'.format(instanceList[x][1])+"| "+'{:20}'.format(instanceList[x][5])+"|"
+        index = next(index for (index, d) in enumerate(instanceList[x][2]) if d["Key"] == "Name")
+	print "| " + '{:22}'.format(instanceList[x][0]) + "| "+'{:30}'.format(instanceList[x][2][index]["Value"])+"| " +'{:18}'.format(instanceList[x][4])+"| "+'{:18}'.format(instanceList[x][3])+"| "+'{:15}'.format(instanceList[x][1])+"| "+'{:20}'.format(instanceList[x][5])+"|"
 
 print '\033[1m' + " ======================================================================================================================================"
 print '\033[0m'
